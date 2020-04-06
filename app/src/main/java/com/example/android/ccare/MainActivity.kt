83d3ab4d1e342c7ee40367d5e474485ec0f8fc25
felix <felixcl91@hotmail.com>
2020-04-06 22:20:51 +0200

    ADD SETONCLICKLISTENER AND DOLOGIN

--- app/src/main/java/com/example/android/ccare/MainActivity.kt
+++ app/src/main/java/com/example/android/ccare/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.android.ccare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val loginRequest = LoginRequest()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        submit.setOnClickListener {
+
+            val userStr = username.text?.toString()
+            val passStr = pass.text?.toString()
+
+            loginRequest.doLogin(userStr, passStr) {success ->
+                val message = if (success) "Success" else "Failure"
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+
     }
 }

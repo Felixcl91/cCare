332b3f247b32d5c5b33154abb9e77eb54dd9144d
felix <felixcl91@hotmail.com>
2020-04-10 17:56:11 +0200

    between activities

--- app/src/main/java/com/example/android/ccare/MainActivity.kt
+++ app/src/main/java/com/example/android/ccare/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android.ccare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -17,11 +18,15 @@ class MainActivity : AppCompatActivity() {
 
             val userStr = username.text?.toString()
             val passStr = pass.text?.toString()
+            val intent: Intent = Intent(this, Responsible::class.java)
 
             loginRequest.doLogin(userStr, passStr) {success ->
                 val message = if (success) "Success" else "Failure"
-                Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+                //Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+               startActivity(intent)
             }
+
+
 
         }
 
